85a0d86acf8a1ed3df0c0f05a8b2d52a699774df
Kleverton Santana Montalvão <tonprofissional9@gmail.com>
2023-01-03 14:33:50 -0300

    Message to test deploy

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,5 +3,6 @@ import { App } from "./App.js";
 // starting server
 const appConnection = new App();
 appConnection.server.listen(appConnection.port, () => {
+    console.log("Mongo connection: " + process.env.MONGODB_PASSWORD);
     console.log("Server succesfully running on port " + appConnection.port);
 });
