8db489aef45ef66dffe016d3a6b3fd424b075940
Kleverton Santana Montalvão <tonprofissional9@gmail.com>
2023-01-03 15:13:12 -0300

    Changed dbConnection import

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,5 +1,5 @@
 import { App } from "./App.js";
-import dbConnection from "./config/dbConnection.js";
+import { dbConnection } from "./config/dbConnection.js";
 
 async function setup() {
     // creating connection with database
